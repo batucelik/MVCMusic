a4df4d0bca978b1382d09c79cf55e78e3bd985f7
batcelik <batuhancelik920@gmail.com>
2016-12-29 22:39:32 +0200

    Bunu ben yolladim

---
+++ MVCMusic/Controllers/StoreController.cs
@@ -0,0 +1,44 @@
+﻿using MVCMusic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCMusic.Controllers
+{
+    public class StoreController : Controller
+    {
+       
+            // GET: Store
+        public ActionResult Index()
+        {
+            var genres = new List<string> { "Rock", "Jazz", "Country", "Pop", "Disco" };
+            var viewModel = new StoreIndexViewModel
+            {
+
+                NumberOfGenres = genres.Count(),
+                Genres = genres
+            };
+
+            return View(viewModel);
+        }
+
+
+        // GET: Browse?genre=Disco
+        public String Browse(string genre)
+        {
+
+            string message = HttpUtility.HtmlEncode("Store.Browse, Genre = " + genre);
+
+            return message;
+        }
+
+        // GET: Store/Details/5
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+	}
+}
---
+++ MVCMusic/Controllers/StoreIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MVCMusic.Controllers
+{
+    class StoreIndexViewModel
+    {
+        public List<string> Genres { get; set; }
+
+        public int NumberOfGenres { get; set; }
+    }
+}
